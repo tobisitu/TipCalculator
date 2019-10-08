d4059942cef058497048172daace36d7a6bf1d40
Tobi Situ <tobisitu@gmail.com>
2019-10-07 20:22:42 -0400

    Updates

---
+++ precache-manifest.911e519abe164317ccebbeed9b4e5084.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801ad3aa55adfe5d1c06fd22a697e5d1",
+    "url": "/TipCalculator/index.html"
+  },
+  {
+    "revision": "53b570ab7795fe403b9c",
+    "url": "/TipCalculator/static/css/main.54e8f20e.chunk.css"
+  },
+  {
+    "revision": "b562a4bd63feeb7d0f1f",
+    "url": "/TipCalculator/static/js/2.e48fe639.chunk.js"
+  },
+  {
+    "revision": "53b570ab7795fe403b9c",
+    "url": "/TipCalculator/static/js/main.ff53d085.chunk.js"
+  },
+  {
+    "revision": "57460b4fbe24bce42928",
+    "url": "/TipCalculator/static/js/runtime-main.7430e054.js"
+  }
+]);
